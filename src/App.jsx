d538b8bc99741fccfb94d36bf58972cb9c27cfe1
Day Sunday <thinman27l@gmail.com>
2025-09-23 19:19:22 +0700

    Updated PublisherDetail, DetailGame, Small Detail and Some Background Adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DetailGame from './pages/DetailGame';
+import PublisherDetail from './pages/PublisherDetail'; // Import PublisherDetail component
+
 function App() {
   return (
     <div>
-      <div>
-        <Routes>
-          {/* Halaman utama */}
-          <Route path="/" element={<Home />} />
-          
-          {/* Halaman detail game berdasarkan ID */}
-          <Route path="/game/:id" element={<DetailGame />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* Halaman utama */}
+        <Route path="/" element={<Home />} />
+
+        {/* Halaman detail game berdasarkan ID */}
+        <Route path="/game/:id" element={<DetailGame />} />
+
+        {/* Halaman detail publisher berdasarkan ID */}
+        <Route path="/publisher/:id" element={<PublisherDetail />} /> {/* Add the route for publisher */}
+      </Routes>
     </div>
   );
 }
